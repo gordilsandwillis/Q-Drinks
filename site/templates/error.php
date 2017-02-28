e3c9847f62b700bf9232f9d222d9efd180f33a25
Matt Gordils <mattgordils@gmail.com>
2017-02-28 14:57:57 -0500

    add error page styling

--- site/templates/error.php
+++ site/templates/error.php
@@ -9,10 +9,7 @@
 		      <p>The page you are looking for has not been found</p>
 	      </div>
 	      <section class="other-mixers grey">
-			    <?php snippet('section-header', ['text' => 'Other Products', 'transition' => 'transition-in']) ?>
-			    <div class="container 4-up-grid">
-			      <?php snippet('mixer-list', ['limit' => 4, 'order' => 'shuffle']) ?>
-			    </div>
+		      <?php snippet('mixer-list', ['limit' => 4, 'order' => 'shuffle']) ?>
 			  </section>
 	      <a href="<?= url() ?>/mixers" rel="home" title="All Mixers" class="btn large">All Mixers</a>
       </div>
