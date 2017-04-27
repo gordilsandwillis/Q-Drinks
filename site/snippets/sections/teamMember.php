150d9649c0765dac1217e97791c99519c53c8e56
Robin Willis <robincwillis@gmail.com>
2017-04-27 16:58:13 -0400

    finish team page

--- site/snippets/sections/teamMember.php
+++ site/snippets/sections/teamMember.php
@@ -1,17 +1,13 @@
 <li class="flex-col team-thumb-toggle">
 	<div class="team-thumb">
 		<a class="team-thumb-link"
-			data-bg-color="rgb(255,250,25)"
-			href="#"
+			data-bg-color="rgb(243,243,245)"
 		>
 			<div class="img-wrap">
 				<img src="<?= $page->image($data->image())->crop(600, 450)->url(); ?>" width="600" height="450" >
 			</div>
 			<h3 class="thumb-title"><?= $data->name()->html() ?></h3>
 		</a>
-	</div>
-	<div class="nipple-wrap">
-		<span class="nipple"></span>
 	</div>
 </li>
 <div class="flex-col inline-team">
@@ -31,7 +27,7 @@
 				</div>
 				<div class="col-8">
 					<div class="team-section">
-						<p><?= $data->description()->html() ?></p>
+						<p class="team-description"><?= $data->description()->html() ?></p>
 					</div>
 				</div>
 			</div>
