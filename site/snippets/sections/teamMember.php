4ea1cb1b93a47ce8068287d1cbf31a82b220d12a
Matt Gordils <mattgordils@gmail.com>
2017-05-02 17:47:57 -0400

    Change team image crop to square

--- site/snippets/sections/teamMember.php
+++ site/snippets/sections/teamMember.php
@@ -4,7 +4,7 @@
 			data-bg-color="rgb(243,243,245)"
 		>
 			<div class="img-wrap">
-				<img src="<?= $page->image($data->image())->crop(600, 450)->url(); ?>" width="600" height="450" >
+				<img src="<?= $page->image($data->image())->crop(600, 600)->url(); ?>" width="600" height="600" >
 			</div>
 			<h3 class="thumb-title"><?= $data->name()->html() ?></h3>
 		</a>
