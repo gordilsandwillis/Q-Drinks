7428e44fe6c48f01fe0d8dd3120753b1568a298e
Matt Gordils <mattgordils@gmail.com>
2020-04-20 13:20:16 -0400

    add back in availability section to about page

--- site/templates/about.php
+++ site/templates/about.php
@@ -4,7 +4,8 @@
 		<nav class="secondary">
 			<div class="container">
 				<ul>
-					<li><a href="#top">Find Q Near You</a></li>
+					<li><a href="#availability">Availability</a></li>
+					<li><a href="#find-q">Find Q Near You</a></li>
 					<li><a href="#contact">Contact Us</a></li>
 					<li><a href="#package">Package Configurations</a></li>
           <li><a href="#leadership">Leadership</a></li>
@@ -12,12 +13,12 @@
 			</div>
 		</nav>
 
-		<?php /*
 		<section id="availability" class="availability grey transition-in">
 			<?php snippet('section-header', ['text' => 'Availability']) ?>
 			<div class="container very-narrow align-center intro-p">
 				<?= $page->availablityIntro()->kirbytext() ?>
 			</div>
+			<?php /*
 			<div class="container narrow">
 				<div class="inline-grid center middle logos break-sm">
 					<?php foreach($page->logos()->toStructure() as $section): ?>
@@ -27,8 +28,8 @@
 					<?php endforeach ?>
 				</div>
 			</div>
+			*/ ?>
 		</section>
-		*/ ?>
 
 		<section id="find-q" class="white transition-in">
 			<?php snippet('section-header', ['text' => 'Find Q Near You']) ?>
